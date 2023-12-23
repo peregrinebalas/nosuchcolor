66bab50398e327ca3121ab69199b4090f8edc9db
Peregrine Balas <peregrinereedbalas@gmail.com>
2023-12-22 23:52:01 -0600

    links

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -20,53 +20,41 @@
   </label>
 {/each}
 
-<!-- export default function Home() {
-  return (
-      <div className="App-home">
-      <h3 style={{color: "white"}}>About Me</h3>
-      <div>
-      <a
-          className="App-link"
-          href="https://github.com/peregrinebalas"
-          target="_blank"
-          rel="noopener noreferrer"
-      >
-          GitHub
-      </a>
-      </div>
-      <div>
-     
-      <a
-          className="App-link"
-          href="https://www.linkedin.com/in/peregrine-balas/"
-          target="_blank"
-          rel="noopener noreferrer"
-      >
-          LinkedIn
-      </a>
-      </div>
-      <div>
-
-      <a
-          className="App-link"
-          href="https://www.instagram.com/peregram_/"
-          target="_blank"
-          rel="noopener noreferrer"
-      >
-          Instagram
-      </a>
-      </div>
-      <div>
-
-      <a
-          className="App-link"
-          href="https://www.facebook.com/peregrine.balas"
-          target="_blank"
-          rel="noopener noreferrer"
-      >
-          Facebook
-      </a>
-      </div>
-      </div>
-  );
-} -->
+<div class="App-home">
+  <div>
+    <a
+      class="App-link"
+      href="https://github.com/peregrinebalas"
+      rel="noopener noreferrer"
+    >
+      GitHub
+    </a>
+  </div>
+  <div>
+    <a
+      class="App-link"
+      href="https://www.linkedin.com/in/peregrine-balas/"
+      rel="noopener noreferrer"
+    >
+      LinkedIn
+    </a>
+  </div>
+  <div>
+    <a
+      class="App-link"
+      href="https://www.instagram.com/peregram_/"
+      rel="noopener noreferrer"
+    >
+      Instagram
+    </a>
+  </div>
+  <div>
+    <a
+      class="App-link"
+      href="https://www.facebook.com/peregrine.balas"
+      rel="noopener noreferrer"
+    >
+      Facebook
+    </a>
+  </div>
+</div>
