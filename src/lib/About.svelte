dfd3b4a0ae02a8f125702a06c88d46e301827272
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-01-31 21:59:56 -0600

    copy change

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -3,10 +3,10 @@
 <!-- <img src="" alt="headshot" /> -->
 
 <p class="about-me">
-  I was raised by a mathematician and an environmentalist who recognized they
-  were a product of their time, distinct from their parents' time. They gave me
-  a hope of understanding the differences and similarities between individuals.
-  I recognize that my sense of humor may be hard to understand as I think
-  miswording things is funny. A sentence may be imperfect, or ultimately
-  unintelligible, but an attempt in understanding is worth a smile.
+  I was raised by a mathematician and an environmentalist. They recognized I
+  would be a product of my time, distinct from theirs. It gave me a hope of
+  understanding the differences and similarities between us. I recognize that my
+  sense of humor may be hard to understand, as I think miswording things is
+  funny. A sentence may be imperfect, or ultimately unintelligible, but an
+  attempt in understanding is worth a smile.
 </p>
