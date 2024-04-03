387a0533de0e75f3def333dce876ab7e0fd7e5cb
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-04-02 23:47:46 -0500

    a bit more style

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -1,24 +1,31 @@
 <script>
   import About from './About.svelte';
-  import { title } from './stores.js';
   import Right from '$lib/assets/right-hand.png';
   import Left from '$lib/assets/left-hand.png';
-  let links = [
+  let entries = [
     {
-      heading: 'information',
-      title: 'About',
-      content: '',
-      url: 'https://github.com/peregrinebalas',
+      component: About,
+      props: {
+        heading: 'information',
+        title: 'About',
+        url: 'https://github.com/peregrinebalas',
+      },
     },
     {
-      heading: 'host',
-      title: 'Socials',
-      url: 'https://www.linkedin.com/in/peregrine-balas/',
+      component: About,
+      props: {
+        heading: 'host',
+        title: 'Socials',
+        url: 'https://www.linkedin.com/in/peregrine-balas/',
+      },
     },
     {
-      heading: 'message',
-      title: 'Contact',
-      url: 'https://www.instagram.com/peregrinebalas/',
+      component: About,
+      props: {
+        heading: 'message',
+        title: 'Contact',
+        url: 'https://www.instagram.com/peregrinebalas/',
+      },
     },
     // {
     //   heading: 'source',
@@ -41,34 +48,24 @@
     //   url: 'https://www.facebook.com/peregrine.balas',
     // },
   ];
-  let currentSection;
-  function handleMouseOver(link) {
-    $title = link.heading.toUpperCase();
-    currentSection = link.title;
-  }
-  function handleMouseLeave() {
-    $title = 'COLOR';
-    currentSection = null;
-  }
+  // let currentSection;
+  // function handleMouseOver(link) {
+  //   $title = link.heading.toUpperCase();
+  //   currentSection = link.title;
+  // }
+  // function handleMouseLeave() {
+  //   $title = 'COLOR';
+  //   currentSection = null;
+  // }
 </script>
 
 <div class="App-home">
   <div class="box">
-    <div class="App-links">
-      {#each links as link}
-        <label>
-          <section>
-            <a
-              class="App-entry"
-              href={link.url}
-              rel="noopener noreferrer"
-              on:mouseover={() => handleMouseOver(link)}
-              on:mouseleave={() => handleMouseLeave()}
-            >
-              {link.title}
-            </a>
-          </section>
-        </label>
+    <div class="App-entries">
+      {#each entries as obj}
+        <section>
+          <svelte:component this={obj.component} props={obj.props} />
+        </section>
       {/each}
     </div>
   </div>
