997e86c4517f6bdc2238e7f0c894458865cf89b5
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-06-26 18:03:35 -0500

    ⊗

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -21,3 +21,24 @@
   or ultimately unintelligible, but an effort in understanding is cause for a
   smile.
 </p>
+
+<div>
+  <p>⊗ Circled Times, tensor product - the relationship between two vectors</p>
+  <p>"Ich lebe mein Leben im wachsenden Ringen." - Rainier Maria Rilke</p>
+
+  <p>◯ infinite expansion of space</p>
+  <p>x infinite contraction of being</p>
+  <p>⊗ the interaction of the two infinites</p>
+
+  <p>
+    "Infinite space is called the goddess NUIT, while the infinitely small and
+    atomic yet omnipresent point is called HADIT. These are unmanifest. One
+    conjunction of these infinites is called RA-HOOR-KHUIT, a Unity which both
+    includes and heads all things." - Aleister Crowley, Liber IV (part III,
+    Magick in theory and practice)
+  </p>
+
+  <p>⊗ The Part of Fortune - luck and worldly success</p>
+
+  <p>⊗ an error, or cancellation</p>
+</div>
