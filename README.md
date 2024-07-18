5e3bebdfb4f0858896f88e92efaf54e5218d4683
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-07-17 23:54:02 -0500

    mv

--- README.md
+++ README.md
@@ -52,6 +52,15 @@ no such...
 - luck
 - function
 
+  I was raised by a mathematician and an environmentalist. They recognized I
+  would be a product of my time, distinct from theirs. It gave me a hope of
+  understanding the differences and similarities between us. Not just among
+  people, but anything that could be considered when entertaining the notion
+  of an entity. I recognize that in my sense of humor. It may be hard to
+  understand. I think miswording things is funny. A sentence may be imperfect,
+  or ultimately unintelligible, but an effort in understanding is cause for a
+  smile.
+
 ## Technologies
 
 svelte?
--- Svelte/src/lib/Definitions.svelte
+++ Svelte/src/lib/Definitions.svelte
@@ -24,15 +24,4 @@
   <p>⊗ an error, or cancellation</p>
 
   <img class="portrait" src={portrait} alt="headshot" />
-
-  <p class="about-me">
-    I was raised by a mathematician and an environmentalist. They recognized I
-    would be a product of my time, distinct from theirs. It gave me a hope of
-    understanding the differences and similarities between us. Not just among
-    people, but anything that could be considered when entertaining the notion
-    of an entity. I recognize that in my sense of humor. It may be hard to
-    understand. I think miswording things is funny. A sentence may be imperfect,
-    or ultimately unintelligible, but an effort in understanding is cause for a
-    smile.
-  </p>
 </div>
